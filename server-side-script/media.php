e2cabf649dd6306b852a8f69b1022a41d57d5594
Hiba Dajani <50335636+daja0003@users.noreply.github.com>
2019-06-24 11:35:59 -0400

    Includes PHP Files

---
+++ server-side-script/media.php
@@ -0,0 +1,6 @@
+    <div class="media">
+        <a class="face" href="http://facebook.com"><i class="fab fa-facebook"></i></a>
+        <a class="tube" href="http://youtube.com"><i class="fab fa-youtube"></i></a>
+        <a class="linked" href="http://linkedin.com"><i class="fab fa-linkedin"></i></a>
+        <a class="inst" href="http://instagram.com"><i class="fab fa-instagram"></i></a>
+    </div>
---
+++ server-side-script/navigation.php
@@ -0,0 +1,19 @@
+<!-- create a navigation -->
+			<nav>
+<!-- create a hamburger icon button to the mobile size -->
+				<button class="hamburger" type="button">
+					<div class="hamburger-box">
+						<span class="hamburger-inner"></span>
+					</div>
+				</button>
+<!-- add a row div to fix the position of the navigation links -->
+				<div class="row">
+<!-- add in the navigation the following links and link them to the class toggle so that the links will appear when ezpanding the hamburger menu -->
+					<ul class="toggle">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Contacts</a></li>
+						<li><a href="#">Blog</a></li>
+					</ul>
+				</div>
+<!-- end navigation -->
+			</nav>
